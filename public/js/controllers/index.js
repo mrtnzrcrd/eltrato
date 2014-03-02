5b9a6d4245d655e504816dd2bad997d358a079ed
victorAlvarez <victor87eg6@gmail.com>
2014-03-02 21:36:34 +0100

    Cambios en index. El controlador controla: Si el usuario no esta logueado y no ha aceptado la geolocalización muestra el formulario de busqueda. Si esta logueado y no tiene activa la geolocalización coge la ubicación predeterminada y le pregunta al usuario si quiere activar la geolocalización. Si no esta logueado y tiene activada la geolocalización coge su ubicación.

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -9,7 +9,44 @@ angular.module('elTrato.system').controller('IndexController', ['$scope', '$http
         $scope.alertOk = true;
         $scope.loading = false;
 
-        if ($rootScope.lng) {
+        console.log($rootScope.lng);
+        console.log(window.user);
+
+        if (window.user != null) {
+            $scope.search = false;
+            var query = window.user.locs[0] + '+' + window.user.locs[1];
+            console.log(query);
+            geolocation.getLocation().then(function (data) {
+                $scope.alerts = [
+                    { type: 'success',
+                        title: 'Muchisimas gracias!',
+                        msg: 'Gracias por activar la geolocalización. Ya puedes disfrutar de todas las ventajas que te ofrece ' +
+                            'elTrato.net. Disfrutalo',
+                        lat: data.coords.latitude}
+                ];
+
+                console.log('Enviado desde Index. Latitude: ' + data.coords.latitude + ' Longitude: ' + data.coords.longitude);
+                $rootScope.lat = data.coords.latitude;
+                $rootScope.lng = data.coords.longitude;
+
+                $rootScope.$broadcast('geo');
+
+                var query = $scope.lng + '+' + $scope.lat;
+                console.log(query);
+                $http.post('/geo', {query: query}).success(function (response) {
+                    $scope.anuncios = response;
+                    $scope.search = false;
+                    $scope.yesAd = true;
+                    $scope.alertOk = false;
+                    $scope.loading = false;
+                });
+            });
+            $http.post('/geo', {query: query}).success(function (response) {
+                $scope.anuncios = response;
+                $scope.yesAd = true;
+                $scope.alertOk = false;
+            });
+        } else if ($rootScope.lng) {
             $scope.search = false;
             var query = $scope.lng + '+' + $scope.lat;
             console.log(query);
@@ -23,12 +60,9 @@ angular.module('elTrato.system').controller('IndexController', ['$scope', '$http
                 { type: 'info',
                     title: 'Disfruta al maximo de elTrato.net',
                     msg: 'Para poder disfrutar al máximo de elTrato.net necesitamos que permitas la geolocalización en tu navegador.' +
-                        'Las ventajas seran maximas, automaticamente calcularemos los productos que están cerca de ti y lo mejor de todo' +
+                        'Las ventajas seran maximas, automaticamente calcularemos los productos que están cerca de ti y lo mejor de todo ' +
                         'es que solo lo tendrás que hacer una vez.' }
             ];
-
-            $scope.loading = true;
-
 
             geolocation.getLocation().then(function (data) {
                 $scope.alerts = [
@@ -39,10 +73,12 @@ angular.module('elTrato.system').controller('IndexController', ['$scope', '$http
                         lat: data.coords.latitude}
                 ];
 
-
                 console.log('Enviado desde Index. Latitude: ' + data.coords.latitude + ' Longitude: ' + data.coords.longitude);
                 $rootScope.lat = data.coords.latitude;
                 $rootScope.lng = data.coords.longitude;
+
+                $rootScope.$broadcast('geo');
+
                 var query = $scope.lng + '+' + $scope.lat;
                 console.log(query);
                 $http.post('/geo', {query: query}).success(function (response) {
@@ -54,6 +90,29 @@ angular.module('elTrato.system').controller('IndexController', ['$scope', '$http
                 });
             });
         }
+
+        $scope.getLocationViaGoogle = function (val) {
+            return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+                params: {
+                    address: val,
+                    sensor: false,
+                    components: 'country:ES'
+                }
+            }).then(function (res) {
+                    var addresses = [];
+                    var geoLocation = [];
+                    angular.forEach(res.data.results, function (item) {
+                        addresses.push(item.formatted_address);
+                        geoLocation.push(item.geometry.location.lng);
+                        geoLocation.push(item.geometry.location.lat);
+                    });
+
+                    $scope.lng = geoLocation[0];
+                    $scope.lat = geoLocation[1];
+
+                    return addresses;
+                });
+        };
 
         $scope.$on('error', function (event, args) {
             console.log(args.title);
