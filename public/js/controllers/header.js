1bfe54775fb256f65f08a32ab904f92d5c1c62be
victorAlvarez <victor87eg6@gmail.com>
2014-03-03 22:27:37 +0100

    Cambios en header cliente y vista header.
    Cuando el usuario se logea aparece el form de busqueda en header, por defecto buscara en la ubicación predeterminada pero cuando se active la geolocalización pasara a buscar en su ubicación actual

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -4,17 +4,36 @@ angular.module('elTrato.system').controller('HeaderController', ['$scope', '$roo
     function ($scope, $rootScope, $location, Global) {
         $scope.global = Global;
 
-        $scope.buscar = function () {
-            console.log("$scope.q: " + $scope.q);
-           // var query = ($scope.q + '').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
-            var query = $scope.q.replace(/\s/g, "+");;
-            $location.path('busqueda/' + query);
-        };
+        $scope.elTrato = true;
+
+        if (window.user) {
+            $scope.lng = window.user.locs[0];
+            $scope.lat = window.user.locs[1];
+
+            $scope.$on('geo', function () {
+                $scope.lng = $rootScope.lng;
+                $scope.lat = $rootScope.lat;
+                console.log('Cuando se activa la geo: ' + $scope.lng + ' ' + $scope.lat);
+            });
+
+            console.log('antes de mirar ubicacion: ' + $scope.lng + ' ' + $scope.lat);
+
+            $scope.buscar = function () {
+                if ($scope.q) {
+                    var query = $scope.q.replace(/\s/g, "+");
+                    var query2 = $scope.lng + '+' + $scope.lat;
+                    //$location.path('busqueda/' + query);
+                    var parameters = {search : query, geo : query2};
+                    $rootScope.$broadcast('searcHeader', parameters);
+                }
+            };
+        }
 
         $rootScope.location = $location;
 
-        if ($rootScope.location.$$absUrl === "http://localhost:3000/#!/") {
+        if ($rootScope.location.$$absUrl === "http://localhost:3000/signup" || $rootScope.location.$$absUrl === "http://localhost:3000/signin") {
             $scope.index = true;
+            $scope.elTrato = false;
         } else {
             $scope.index = false;
         }
