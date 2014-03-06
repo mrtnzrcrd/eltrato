a71b58e3cd12abc078ce286a31b6b5fdd711bbe6
victorAlvarez <victor87eg6@gmail.com>
2014-03-06 14:43:39 +0100

    Añadido nuevo service geocoder.
    Nos proporciona la localidad de cada latitude y longitude. Para integrarlo hace falta un bower update

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('elTrato', ['ngCookies', 'ngResource','ngRoute', 'ui.bootstrap', 'ui.route', 'elTrato.system', 'elTrato.anuncios',
-    'geolocation', 'elTrato.signup', 'elTrato.gestion', 'elTrato.mensajes', 'elTrato.cuenta']);
+    'geolocation', 'elTrato.signup', 'elTrato.gestion', 'elTrato.mensajes', 'elTrato.cuenta' , 'elTrato.geocoder']);
 
 angular.module('elTrato.system', ['geolocation']);
 angular.module('elTrato.anuncios', ['ui.map', 'ui.event']);
@@ -9,3 +9,4 @@ angular.module('elTrato.signup', ['ui.map', 'ui.event']);
 angular.module('elTrato.gestion', ['ngAnimate']);
 angular.module('elTrato.cuenta', []);
 angular.module('elTrato.mensajes', []);
+angular.module('elTrato.geocoder', ['ngStorage']);
