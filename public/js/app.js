c83f3aa971f7834cf9f5dcafbea53fb404489383
victorAlvarez <victor87eg6@gmail.com>
2014-03-13 14:46:27 +0100

    Cambios en bower. Realizar bower update
    Añadida libreria moment.js y angular-moment

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@
 angular.module('elTrato', ['ngCookies', 'ngResource','ngRoute', 'ui.bootstrap', 'ui.route', 'elTrato.system', 'elTrato.anuncios',
     'geolocation', 'elTrato.signup', 'elTrato.gestion', 'elTrato.mensajes', 'elTrato.cuenta' , 'elTrato.geocoder']);
 
-angular.module('elTrato.system', ['geolocation']);
+angular.module('elTrato.system', ['geolocation', 'angularMoment']);
 angular.module('elTrato.anuncios', ['ui.map', 'ui.event']);
 angular.module('elTrato.signup', ['ui.map', 'ui.event']);
 angular.module('elTrato.gestion', ['ngAnimate']);
