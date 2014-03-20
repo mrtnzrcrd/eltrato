511614f88980fbfdb5274af826c4dd368b3e4a27
victorAlvarez <victor87eg6@gmail.com>
2014-03-20 12:04:12 +0100

    creación de directivas isFavorite, isTrueque y photos

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,23 +1,23 @@
 'use strict';
 /*
-window.angular.module('elTrato.upload2', [])
-    .directive('upload', [function () {
-        return {
-            restrict: 'A',
-            link: function (scope, elem, attrs) {
-                var reader = new FileReader();
-                reader.onload = function (e) {
-                    var image = e.target.result;
-                    scope.$emit("fileSelected", { image: image, files : elem[0].files[0]});
-                }
+ window.angular.module('elTrato.upload2', [])
+ .directive('upload', [function () {
+ return {
+ restrict: 'A',
+ link: function (scope, elem, attrs) {
+ var reader = new FileReader();
+ reader.onload = function (e) {
+ var image = e.target.result;
+ scope.$emit("fileSelected", { image: image, files : elem[0].files[0]});
+ }
 
-                elem.on('change', function () {
-                    reader.readAsDataURL(elem[0].files[0]);
-                });
-            }
-        };
-    }])
-*/
+ elem.on('change', function () {
+ reader.readAsDataURL(elem[0].files[0]);
+ });
+ }
+ };
+ }])
+ */
 
 // It is attached to an element that catches the event drop file
 angular.module('elTrato.anuncios').directive('ngFileDrop', [ '$fileUploader', function ($fileUploader) {
@@ -98,18 +98,77 @@ angular.module('elTrato.system').directive('myInput', function () {
 });
 
 /*angular.module('elTrato.system').directive("loadingIndicator", function() {
+ return {
+ restrict : "A",
+ template: "<div id='loading' style='display: inline-block;'><img src='../img/icons/ajax-loader.gif'><p>Comprobando favorito</p></div>",
+ link : function(scope, element, attrs) {
+ scope.$on("loading-started", function(e) {
+ element.css({"display" : ""});
+ });
+
+ scope.$on("loading-complete", function(e) {
+ element.css({"display" : "none"});
+ });
+
+ }
+ };
+ });*/
+
+angular.module('elTrato.system').directive('isFavorite', ['$compile', function ($compile) {
     return {
-        restrict : "A",
-        template: "<div id='loading' style='display: inline-block;'><img src='../img/icons/ajax-loader.gif'><p>Comprobando favorito</p></div>",
-        link : function(scope, element, attrs) {
-            scope.$on("loading-started", function(e) {
-                element.css({"display" : ""});
-            });
+        restrict: 'E',
+        link: function (scope, element, attrs) {
+            var favorite = attrs.favorite;
+            var favorites = window.user.favorites;
+            var result = favorites.indexOf(favorite);
+            var tag = '';
 
-            scope.$on("loading-complete", function(e) {
-                element.css({"display" : "none"});
-            });
+            if (result != -1) {
+                tag = '<a class="btn btn-warning btn-sm" role="button" tooltip="Favorito"> ' +
+                    '<span class="glyphicon glyphicon-star"></span> </a>'
+            } else {
+                tag = '<a id="{{anuncio._id}}" class="btn btn-favorite btn-sm" role="button" ' +
+                    'ng-click="favoritosGeneral(anuncio._id)" tooltip="Añadir a favoritos"> ' +
+                    '<span class="glyphicon glyphicon-star-empty"></span> </a>'
+            }
 
+            element.append($compile(tag)(scope));
         }
     };
-});*/
+}]);
+
+angular.module('elTrato.system').directive('isTrueque', ['$compile', function ($compile) {
+    return {
+        restrict: 'E',
+        link: function (scope, element, attrs) {
+            var trueque = attrs.trueque;
+            var tag = '';
+
+            if (trueque == "true") {
+                tag = '<a href="#" class="btn btn-deal btn-sm" role="button" tooltip="Contraoferta" ' +
+                    'data-ng-repeat="trueque in anuncio.opciones" ng-switch on="trueque.trueque"> ' +
+                    '<span class="glyphicon glyphicon-transfer"></span> Contraoferta </a>'
+            }
+
+             element.append($compile(tag)(scope));
+        }
+    };
+}]);
+
+angular.module('elTrato.system').directive('photos', ['$compile', function ($compile) {
+    return {
+        restrict: 'E',
+        link: function (scope, element, attrs) {
+            var photos = attrs.numphotos;
+            photos = JSON.parse(photos);
+            photos = photos.length;
+            console.log(photos);
+
+            var  tag = '<div class="col-md-12 numPhotos">' +
+                            '<span class="glyphicon glyphicon-camera"> </span> ' + photos +
+                       '</div>';
+
+            element.append($compile(tag)(scope));
+        }
+    };
+}]);
